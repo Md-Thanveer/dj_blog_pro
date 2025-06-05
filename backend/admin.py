8bc67f8a771849216fd574a5e0ac5ca5e81ba746
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-06-05 11:34:54 +0530

    Post admin added

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from backend.models import Tag
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
